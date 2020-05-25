14b303dac75eb8c1300c1c576cf915acdad5656f
Stuart Berg <bergs@janelia.hhmi.org>
2020-05-24 23:00:45 -0400

    Split outputs into separate packages for libngspice and the ngspice executable, and a third metapackage for both

---
+++ recipe/build_common.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -x
+
+# Used by autotools AX_PROG_CC_FOR_BUILD
+export CC_FOR_BUILD=${CC}
+
+./autogen.sh
+
+configure_args=(
+  --prefix=${PREFIX}
+  --enable-xspice
+  --disable-debug
+  --enable-cider
+  --with-readline=yes
+  --enable-openmp
+  --enable-relpath
+
+  # Not enabled for now:
+  #  --enable-adms
+)
+
+if [[ ! -z "${BUILD_NGSPICE_LIB}" && ! -z "${BUILD_NGSPICE_EXE}" ]]; then
+  2>&1 echo "Set either BUILD_LIB or BUILD_EXE"
+  exit 1
+fi
+
+if [[ ! -z "${BUILD_NGSPICE_LIB}" ]]; then
+  #
+  # build libngspice.dylib
+  #
+  mkdir release-lib && cd release-lib
+  ../configure "${configure_args[@]}" --with-ngshared
+  make -j${CPU_COUNT}
+  make install
+  cd -
+fi
+
+if [[ ! -z "${BUILD_NGSPICE_EXE}" ]]; then
+  #
+  # build ngspice executable
+  #
+  mkdir release-bin && cd release-bin
+  ../configure "${configure_args[@]}" --with-x
+  make -j${CPU_COUNT}
+  make install
+  cd -
+fi
---
+++ recipe/build_lib.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+BUILD_NGSPICE_LIB=1 ${RECIPE_DIR}/build_common.sh
+exit $?
