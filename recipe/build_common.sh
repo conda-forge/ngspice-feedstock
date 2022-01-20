a1f18c00709a31da8395f6cab43db98881ce7ba0
Stuart Berg <bergs@janelia.hhmi.org>
2022-01-20 11:56:31 -0500

    Patch xspice/icm LDFLAGS on MacOSX

--- recipe/build_common.sh
+++ recipe/build_common.sh
@@ -20,6 +20,8 @@ configure_args=(
   #  --enable-adms
 )
 
+export LDFLAGS="${LDFLAGS} -m64"
+
 if [[ ! -z "${BUILD_NGSPICE_LIB}" && ! -z "${BUILD_NGSPICE_EXE}" ]]; then
   2>&1 echo "Set either BUILD_NGSPICE_LIB or BUILD_NGSPICE_EXE"
   exit 1
@@ -30,7 +32,7 @@ if [[ ! -z "${BUILD_NGSPICE_LIB}" ]]; then
   # build libngspice.dylib
   #
   mkdir release-lib && cd release-lib
-  ../configure "${configure_args[@]}" --with-ngshared
+  ../configure "${configure_args[@]}" --with-ngshared LDFLAGS="${LDFLAGS}"
   make -j${CPU_COUNT}
   make install
   cd -
@@ -41,7 +43,7 @@ if [[ ! -z "${BUILD_NGSPICE_EXE}" ]]; then
   # build ngspice executable
   #
   mkdir release-bin && cd release-bin
-  ../configure "${configure_args[@]}" --with-x
+  ../configure "${configure_args[@]}" --with-x LDFLAGS="${LDFLAGS}"
   make -j${CPU_COUNT}
   make install
   cd -
